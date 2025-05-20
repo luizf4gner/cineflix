9077c9ff418d6209f2a7a525e31f4d282ac6038e
luizf4gner <luizfagnersilva43@gmail.com>
2025-05-20 18:20:46 -0300

    Update assinaturasRepository.py

--- cineflixBackend/Repositories/assinaturasRepository.py
+++ cineflixBackend/Repositories/assinaturasRepository.py
@@ -10,4 +10,34 @@ class assinaturaReposiory():
 
   def getById(self, id):
     query = "select * from assinaturas where id = %s"
-    return self.baseRepository.fetch(query, (id))
+    return self.baseRepository.fetch(query, (id,))
+
+  def create(self):
+    query = """
+        insert into assinaturas 
+        set usuario_id = %s, data_inicial = %s, data_final = %s
+        where id = %s
+    """
+    params = (
+      data["usuario_id"],
+      data["data_inicial"],
+      data["data_final"],
+      id
+    )
+    return self.baseRepository.execute(query, params)
+
+  def update(self, data, id):
+    query = """
+        update assinaturas (usuario_id, data_inicial, data_final)
+        values (%s, %s, %s)
+    """
+    params = (
+      data["usuario_id"],
+      data["data_inicial"],
+      data["data_final"]
+    )
+    return self.baseRepository.execute(query, params)
+
+  def delete(self, id):
+    query = "delete from assinaturas where id = %s"
+    return self.baseRepository.execute(query, (id,))
